bc0446d6a9e4ea240bdd76afc0d904c21342a51c
Yomikoda <yomikoda@gmail.com>
2017-03-22 20:13:38 +0100

    tabmenu2 + playerkinda

--- js/main.js
+++ js/main.js
@@ -35,27 +35,27 @@ $(function () {
         })
     });
     /**CALENDAR**/
-    
-    
- var example = flatpickr('#flatpickr');
-    
 
-    
-    
+
+    var example = flatpickr('#flatpickr');
+
+
+
+
     /**SLIDER**/
     /** SOUNDCLOUNDY THINGY **/
-    
+
     var wavesurfer = WaveSurfer.create({
         container: '#waveform',
         waveColor: 'rgba(255,255,255,0.2)',
         progressColor: 'white',
         cursorWidth: 0,
-        barWidth : 3
+        barWidth: 3
 
     });
 
     wavesurfer.load('https://wavesurfer-js.org/example/media/demo.wav');
-    
+
     var formatTime = function (time) {
         return [
         Math.floor((time % 3600) / 60), // minutes
@@ -70,26 +70,155 @@ $(function () {
 
     // Show clip duration
     wavesurfer.on('ready', function () {
-        
-//        wavesurfer.playPause();
-        
-        $('#timewave').click(function(){
+
+        //        wavesurfer.playPause();
+
+        $('#timewave').click(function () {
             wavesurfer.playPause();
         });
     });
 
 
-/** GRAPH **/
-    
-/** RANGE SLIDER **/
-    
-/** TABS **/
-    
+    /** GRAPH **/
+
+    /** RANGE SLIDER **/
+
+    /** TABS **/
+
     $('.tab').click(function () {
         $(this).toggleClass('clicked-tab');
-        
-    });
-    
-    
 
+    });
+
+    $('.simpletab').click(function () {
+        $(this).toggleClass('selectedsimpletab');
+
+    });
+
+    /** AUDIO ?? **/
+
+
+    var music = document.getElementById('music'); // id for audio element
+    var duration = music.duration; // Duration of audio clip, calculated here for embedding purposes
+    var pButton = document.getElementById('pButton'); // play button
+    var playhead = document.getElementById('playhead'); // playhead
+    var timeline = document.getElementById('timeline'); // timeline
+
+    // timeline width adjusted for playhead
+    var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
+
+    // play button event listenter
+    pButton.addEventListener("click", play);
+
+    // timeupdate event listener
+    music.addEventListener("timeupdate", timeUpdate, false);
+
+    // makes timeline clickable
+    timeline.addEventListener("click", function (event) {
+        moveplayhead(event);
+        music.currentTime = duration * clickPercent(event);
+    }, false);
+
+    // returns click as decimal (.77) of the total timelineWidth
+    function clickPercent(event) {
+        return (event.clientX - getPosition(timeline)) / timelineWidth;
+    }
+
+    // makes playhead draggable
+    playhead.addEventListener('mousedown', mouseDown, false);
+    window.addEventListener('mouseup', mouseUp, false);
+
+    // Boolean value so that audio position is updated only when the playhead is released
+    var onplayhead = false;
+
+    // mouseDown EventListener
+    function mouseDown() {
+        onplayhead = true;
+        window.addEventListener('mousemove', moveplayhead, true);
+        music.removeEventListener('timeupdate', timeUpdate, false);
+    }
+
+    // mouseUp EventListener
+    // getting input from all mouse clicks
+    function mouseUp(event) {
+        if (onplayhead == true) {
+            moveplayhead(event);
+            window.removeEventListener('mousemove', moveplayhead, true);
+            // change current time
+            music.currentTime = duration * clickPercent(event);
+            music.addEventListener('timeupdate', timeUpdate, false);
+        }
+        onplayhead = false;
+    }
+    // mousemove EventListener
+    // Moves playhead as user drags
+    function moveplayhead(event) {
+        var newMargLeft = event.clientX - getPosition(timeline);
+
+        if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
+            playhead.style.marginLeft = newMargLeft + "px";
+        }
+        if (newMargLeft < 0) {
+            playhead.style.marginLeft = "0px";
+        }
+        if (newMargLeft > timelineWidth) {
+            playhead.style.marginLeft = timelineWidth + "px";
+        }
+    }
+
+    // timeUpdate
+    // Synchronizes playhead position with current point in audio
+    function timeUpdate() {
+        var playPercent = timelineWidth * (music.currentTime / duration);
+        playhead.style.marginLeft = playPercent + "px";
+        if (music.currentTime == duration) {
+            pButton.className = "";
+            pButton.className = "play";
+        }
+    }
+
+    //Play and Pause
+    function play() {
+        // start music
+        if (music.paused) {
+            music.play();
+            // remove play, add pause
+            pButton.className = "";
+            pButton.className = "pause";
+        } else { // pause music
+            music.pause();
+            // remove pause, add play
+            pButton.className = "";
+            pButton.className = "play";
+        }
+    }
+
+    // Gets audio file duration
+    music.addEventListener("canplaythrough", function () {
+        duration = music.duration;
+    }, false);
+
+    // getPosition
+    // Returns elements left position relative to top-left of viewport
+    function getPosition(el) {
+        return el.getBoundingClientRect().left;
+    }
+    
+    function readableDuration(seconds) {
+    sec = Math.floor( seconds );    
+    min = Math.floor( sec / 60 );
+    min = min >= 10 ? min : '0' + min;    
+    sec = Math.floor( sec % 60 );
+    sec = sec >= 10 ? sec : '0' + sec;    
+    return min + ':' + sec;
+}
+    ;
+    
+    
+    $('#trackduration').text(readableDuration(duration));
+    
+    
+    
+    
+    
 });
